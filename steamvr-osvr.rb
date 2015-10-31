6b69816936a319f035cd45e026b1796b368fbbbd
David Ryskalczyk <d235j.1@gmail.com>
2015-10-30 22:41:03 -0400

    Add SteamVR-OSVR 0.1 release

--- steamvr-osvr.rb
+++ steamvr-osvr.rb
@@ -1,6 +1,7 @@
 class SteamvrOsvr < Formula
   desc "SteamVR-OSVR"
   homepage "https://github.com/OSVR/SteamVR-OSVR"
+  url "https://github.com/OSVR/SteamVR-OSVR.git", :tag => "v0.1", :revision => "c4aa9ec4d39bee8375c633f50aa465e64d2ea0c6"
   head "https://github.com/OSVR/SteamVR-OSVR.git"
 
   depends_on "cmake" => :build
