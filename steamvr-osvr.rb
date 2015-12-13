a5ea5bf29ea0715975531290e5a36d495798ed4a
David Ryskalczyk <d235j.1@gmail.com>
2015-12-12 20:08:30 -0500

    oops (again...)

--- steamvr-osvr.rb
+++ steamvr-osvr.rb
@@ -10,12 +10,13 @@ class SteamvrOsvr < Formula
   option :universal
 
   def install
+    args = std_cmake_args
     if build.universal?
       ENV.universal_binary
       args << "-DCMAKE_OSX_ARCHITECTURES=#{Hardware::CPU.universal_archs.as_cmake_arch_flags}"
     end
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args
+      system "cmake", "..", *args
       system "make", "install"
     end
   end
