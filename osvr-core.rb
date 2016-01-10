e0fa3c9226c4d8490490500d2819fe7ae4125a3c
David Ryskalczyk <d235j.1@gmail.com>
2016-01-09 20:01:44 -0500

    Ugly workaround to allow osvr_server to work on Mac

--- osvr-core.rb
+++ osvr-core.rb
@@ -19,6 +19,14 @@ class OsvrCore < Formula
       args << "-DCMAKE_OSX_ARCHITECTURES=#{Hardware::CPU.universal_archs.as_cmake_arch_flags}"
     end
 
+    # work around https://github.com/OSVR/OSVR-Core/issues/321
+    cd "vendor/vrpn/submodules/hidapi" do
+      system "git reset --hard c7f35c3a462aa5eec16d6e2e521e121aa7734bf9"
+      system "git remote set-url origin https://github.com/signal11/hidapi.git"
+      system "git fetch origin"
+      system "git checkout d17db57b9d4354752e0af42f5f33007a42ef2906"
+    end
+
     mkdir "build" do
       system "cmake", "..", *args
       system "make", "install"
